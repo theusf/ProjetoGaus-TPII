c8fb74bbaae45ba5de12d597859e6ea42bee363a
Leonardo <cyndrasilgames@gmail.com>
2019-10-23 21:16:43 -0300

    Update Equacao.java
    Adicionei equals

--- Equacao.java
+++ Equacao.java
@@ -14,13 +14,13 @@ public Equacao(int tamanho) throws Exception{
 
 public void addIncog(Double x) throws Exception {
     
-    this.valores[qtd] = x;
-    qtd++;
+    this.valores[qtd] = x;  // se qtd == 0, é o primeiro valor do vetor 
+    qtd++;     
 }
 
 public Double getResultado (){
 
-    return new Double(this.valores[qtd-1]);
+    return new Double(this.valores[qtd-1]);    //qtd-1 pois qtd começa com 0(vetor)
 
 }
 
@@ -46,6 +46,32 @@ public Double getIncog (double pos){
 public int getQtdEq ()
 {
 	return this.qtd;
+}
+
+public boolean equals(Object obj)
+{
+	if(this == obj)
+	return true;
+	
+	if(obj == null)
+	return false;
+	
+	if(obj.getClass() != this.getClass())
+	return false;
+	
+	Equacao eqObj = (Equacao) obj;
+	
+	if(this.qtd != eqObj)
+	return false; 
+
+	for(int i = 0; i < this.qtd; i++)			//qtd retorna a quantidade valores q equacao tem;     0 3 1 = 28 tem 4 valores
+	{
+		if(this.valores[i] != eqObj.valores[i])
+		return false;
+	}
+	
+	return true;
+	
 }
 
   
